242c62b04239cb7a16440650b2dc8bb4a59f572b
xuliwen <xulw@bagtree.cn>
2016-11-24 10:23:20 +0800

    Initial Commit

---
+++ LWShared/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LWShared
+//
+//  Created by bagtree on 16/11/24.
+//  Copyright © 2016年 wen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LWShared/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LWShared
+//
+//  Created by bagtree on 16/11/24.
+//  Copyright © 2016年 wen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
